f86028599dc75adc1f60a68ae2044b409a87af60
tarunfy <tarunsharma8920@gmail.com>
2022-05-09 16:31:13 +0530

    added scroll trigger + locomotive scroll + tl animation, in project section

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,21 +1,136 @@
-import React from "react";
+import React, { useEffect } from "react";
+import hirely from "../../assets/images/Hirely.svg";
+import easyStaff from "../../assets/images/EasyStaff.svg";
+import formify from "../../assets/images/Formify.svg";
+import sigmaRules from "../../assets/images/SigmaRules.svg";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
+  useEffect(() => {
+    let containers = document.querySelectorAll(".container");
+
+    containers.forEach((container) => {
+      let imgContainer = container.querySelector(".img-container");
+      let img = container.querySelector("img");
+      gsap.delayedCall(1, () => {
+        const tl = gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: container,
+              start: "top 60%",
+              end: "top top+=100px",
+              scroller: "#main-container",
+              toggleActions: "play none resume pause",
+              markers: true,
+              scrub: 1,
+              anticipatePin: true,
+            },
+          })
+          .set(container, {
+            duration: 0,
+            visibility: "visible",
+            ease: "Power2.easeInOut",
+          })
+          .to(imgContainer, {
+            duration: 1,
+            width: "0%",
+            ease: "Power2.easeInOut",
+          })
+          .from(
+            img,
+            {
+              duration: 1.4,
+              scale: 1.6,
+              ease: "Power2.easeInOut",
+            },
+            "=-1.6"
+          );
+      });
+    });
+  });
   return (
     <div
-      className="h-[2000px] w-full px-44 bg-[#420fe7]"
+      className="w-full px-44 bg-[#420fe7]"
       data-scroll-section
       id="projects"
     >
       <h1
         data-scroll
         data-scroll-delay="0.1"
-        data-scroll-speed="6"
-        className="text-white/70 text-[9rem] leading-[9rem] cursor-default"
+        data-scroll-speed="4"
+        className="text-white/70 z-40 text-[9rem] leading-[9rem] cursor-default"
         id="project-heading"
       >
         Featured Projects
       </h1>
+      <div className="space-y-10 z-30">
+        <div
+          className="container"
+          data-scroll
+          data-scroll-delay="0.1"
+          data-scroll-speed="4"
+        >
+          <div className="w-[940px] h-[460px] relative overflow-hidden">
+            <img
+              src={hirely}
+              alt="project image"
+              className="absolute w-[940px]"
+            />
+            <div className="img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full"></div>
+          </div>
+        </div>
+
+        <div
+          className="container"
+          data-scroll
+          data-scroll-delay="0.1"
+          data-scroll-speed="4"
+        >
+          <div className="w-[940px] h-[460px] relative overflow-hidden">
+            <img
+              src={formify}
+              alt="project image"
+              className="absolute w-[940px]"
+            />
+            <div className="img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full"></div>
+          </div>
+        </div>
+
+        <div
+          className="container"
+          data-scroll
+          data-scroll-delay="0.1"
+          data-scroll-speed="4"
+        >
+          <div className="w-[940px] h-[460px] relative overflow-hidden">
+            <img
+              src={easyStaff}
+              alt="project image"
+              className="absolute w-[940px]"
+            />
+            <div className="img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full"></div>
+          </div>
+        </div>
+
+        <div
+          className="container"
+          data-scroll
+          data-scroll-delay="0.1"
+          data-scroll-speed="4"
+        >
+          <div className="w-[940px] h-[460px] relative overflow-hidden">
+            <img
+              src={sigmaRules}
+              alt="project image"
+              className="absolute w-[940px]"
+            />
+            <div className="img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full"></div>
+          </div>
+        </div>
+      </div>
       {/* <span class="lerp-wrap" data-scroll id="hero-title" className="text-8xl">
         <span
           data-scroll
@@ -57,8 +172,8 @@ const Projects = () => {
         >
           T
         </span>
-      </span>
-      <div className="transform rotate-6 skew-x-12">
+      </span> */}
+      {/* <div className="transform rotate-6 skew-x-12">
         <div
           data-scroll
           data-scroll-direction="horizontal"
@@ -69,8 +184,8 @@ const Projects = () => {
             Social Media — Email Marketing — Digital Advertising
           </span>
         </div>
-      </div>
-      <div className="transform -rotate-6 -skew-x-12 mt-40">
+      </div> */}
+      {/* <div className="transform -rotate-6 -skew-x-12 mt-40">
         <div
           data-scroll
           data-scroll-direction="horizontal"
--- src/hooks/useLocoScroll.js
+++ src/hooks/useLocoScroll.js
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 import LocomotiveScroll from "locomotive-scroll";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const useLocoScroll = () => {
   useEffect(() => {
@@ -8,10 +12,33 @@ export const useLocoScroll = () => {
     const locoScroll = new LocomotiveScroll({
       el,
       smooth: true,
-      multiplier: 1,
-      class: "reveal",
       tablet: { smooth: true },
       smartphone: { smooth: true },
     });
+
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main-container", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main-container").style.transform
+        ? "transform"
+        : "fixed",
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
   }, []);
 };
