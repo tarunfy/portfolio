416b3d92c08ebcf58ce4c3c486e512e5501e7da1
tarunfy <tarunsharma8920@gmail.com>
2022-11-19 02:21:01 +0530

    add learningcafe project and update resume file

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,6 +4,7 @@ import easyStaff from "../../assets/images/EasyStaff.svg";
 import formify from "../../assets/images/Formify.svg";
 import bloggy from "../../assets/images/Bloggy.svg";
 import planitio from "../../assets/images/Planitio.svg";
+import learningCafe from "../../assets/images/LearningCafe.svg";
 import { BsGithub, BsArrowRight } from "react-icons/bs";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -122,6 +123,50 @@ const Projects = () => {
         Featured Projects
       </h1>
       <div className="sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30">
+        {/* LearningCafe */}
+        <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
+          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+            <img
+              src={learningCafe}
+              alt="project image"
+              className="absolute w-full object-cover h-full img"
+            />
+            <div className="img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full"></div>
+          </div>
+
+          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
+            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+              <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden bg-[#420fe7] ">
+                <div className="content-line-inner">Learning Cafe</div>
+              </div>
+            </h1>
+            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl  sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
+              A platform for posting courses which are designed as per the
+              Industry demand with full Career Assistance.
+            </p>
+            <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
+              <div className="github-btn">
+                <a
+                  className="icon "
+                  target="_blank"
+                  href="https://github.com/tarunfy/learningcafe"
+                >
+                  <BsGithub className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out repo text-white/90 hover:text-white" />
+                </a>
+              </div>
+              <div className="liveDemo-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://learningcafe.co.in/"
+                >
+                  <BsArrowRight className="link lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-rotate-[45deg] hover:text-white transition duration-200 ease-in-out text-white/90" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* Hirely */}
         <div className="container lg:space-x-2  flex flex-col-reverse lg:flex-row lg:justify-between  lg:items-center items-start invisible ">
           <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3 ">
